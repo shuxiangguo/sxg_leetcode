67e4ee3f417c77d3d7819491f88eb7f8d537b0c4
shuxiangguo <shuxiangguo_li@163.com>
2019-11-06 16:50:14 +0800

    add 290

---
+++ 290.单词规律.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=290 lang=cpp
+ *
+ * [290] 单词规律
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool wordPattern(string pattern, string str) {
+        
+    }
+};
+// @lc code=end
+
